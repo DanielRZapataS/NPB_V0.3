e65111675464a5bc2a241625a0d66a2b52e18f9d
Daniel Ricardo Zapata Sanabria <DZAPAT3@banbogota.com.co>
2018-10-25 16:51:16 -0500

    documentation function

--- Scripts/orquestador.R
+++ Scripts/orquestador.R
@@ -2,7 +2,7 @@
 # the requested function
 # - Creation: It launchs the creation scripts to generate the master tables to train the new models
 # - Scoring: It launchs the scoring scripts either the new leads or to check the performance
-# - Training: It launchs the training scripts to create new models (either Cartera or Tarjeta)
+# - Training: It launchs the training scripts to create new models (either tcredito or crediservice)
 # It gives back an error if the parameter if not "Creation", "Scoring" or "Training"
 switch(
   job,
