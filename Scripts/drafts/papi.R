d14613cdae690871e8cf130b7a5da1b3908c6e3d
Daniel Zapata <danielrzapatas@gmail.com>
2018-11-17 16:58:53 -0500

    First draft of Next product to buy model

---
+++ Scripts/drafts/papi.R
@@ -0,0 +1,155 @@
+## Starting 
+
+# Clean environment
+rm(list=ls())
+gc()
+# Disable scientific notation
+options(scipen=999)
+# Change prompt
+options(prompt="NPB_V0.3> ", continue=" ") 
+
+# Load utilities functions (change wd, auxiliary scripts...)
+source("Scripts/utiles.R")
+# Set up paths
+set_environment()  
+
+# install.packages("foreach")
+library(foreach)
+library(doMC)
+library(Metrics)
+
+# load model predictins on test 
+models <-
+  c(
+    "20181101_tcredito",
+    "20181031_crediservice",
+    "20181102_ahorros",
+    "20181108_cdt",
+    "20181107_vehiculo",
+    "20181107_libranza",
+    "20181101_libredestino",
+    "20181101_nomina",
+    "20181108_vivienda"
+  )
+
+models_files <- list.files(models_path)
+models_files <- models_files[models_files %in% models]
+products <- sapply(strsplit(models_files, "_"), "[[", 2)
+models_files_table <- data.table(models = models_files, products = products) 
+
+results <- list()
+for(i in 1:length(models_files) ){
+  results[[i]] <- fread(
+    os.path.join(models_path, models_files[i], "pred_test.csv"))
+}
+# results
+
+
+for(i in 1:length(products)){
+  results[[i]]$product <-  products[i]
+}
+preds <- rbindlist(results)
+
+preds <-
+  dcast(preds, llave + periodo ~ product, value.var = c("target", "pred"))
+# preds
+targets <- names(preds)[grepl("target_", names(preds))]
+preds[, count := rowSums(preds[, mget(targets)])]
+probs <- names(preds)[grepl("pred_", names(preds))]
+preds[, prob_mean :=  rowSums(preds[, mget(probs)])]
+preds[, prob_mean := prob_mean / 9]
+qu <- quantile(preds$prob_mean, probs=seq(0,1, by=1/9))
+preds[, prob_quantil := cut(preds$prob_mean, breaks=qu,
+                            labels=names(qu)[-1],
+                            include.lowest=TRUE)]
+
+preds[order(-count)]
+preds[, .N, by = count]
+
+preds[order(-prob_mean)]
+preds[, .N, by = prob_quantil]
+
+
+############ 
+
+actual <- list(c('a', 'b'), c('a'), c('x', 'y', 'b'))
+actual <- list()
+predicted <- list(c('a', 'c', 'd'))
+mapk(2, actual, predicted)
+
+k <- 1
+
+products <- names(preds)[grepl("pred_", names(preds))]
+products <- sapply(strsplit(products, "_"), "[[", 2)
+
+preds
+cols <- c("llave", targets)
+products_acq <- preds[, mget(cols)]
+names(products_acq)[-1] <- products
+
+products_acq  <- melt(products_acq , id.vars = c("llave"))
+products_acq <- products_acq[products_acq$value > 0]
+setkey(products_acq, llave)
+products_acq
+# products_acq <- dcast(products_acq, llave ~ variable, value.var = "value")
+
+cols <- c("llave", probs)
+
+products_pred <- preds[, mget(cols)]
+names(products_pred)[-1] <- products
+products_pred  <- melt(products_pred , id.vars = c("llave"))
+products_pred[, pred_order := rank(-value), by = .(llave) ]
+
+setkey(products_pred, llave, pred_order)
+head(products_pred, 20)
+
+## 
+
+products_acq$llave[products_acq$llave %!in% products_pred$llave]
+loss_clients <- products_pred$llave[products_pred$llave %!in% products_acq$llave]
+loss_clients <- unique(loss_clients)
+## getting clientes loss on previous filter 
+
+products_acq <- rbindlist(list(products_acq,
+                               data.table( 
+                                 llave = loss_clients,
+                                 variable = "", 
+                                 value = NA
+                                 ) ))
+products_pred$llave[products_pred$llave %!in% products_acq$llave]
+
+setkey(products_acq, llave)
+products_acq
+
+## APK 
+
+products_rec <- merge(products_acq[, .(llave, acq = variable)],
+                      products_pred[, .(llave, pred = variable)],
+                      by = "llave" )
+
+apk <- products_rec[, .(apk = apk(1, acq, pred)), by = llave]
+mean(apk$apk)
+
+products_rec <- merge(products_acq[value == 1, .(llave, acq = variable)],
+                      products_pred[, .(llave, pred = variable)],
+                      by = "llave" )
+apk <- products_rec[, .(apk = apk(1, acq, pred)), by = llave]
+mean(apk$apk)
+
+## 
+products_rec <- merge(products_acq[, .(llave, acq = variable)],
+                      products_pred[, .(llave, pred = variable)],
+                      by = "llave" )
+mape <- c()
+for(i in 1:9){
+  apk <- products_rec[, .(apk = apk(i, acq, pred)), by = llave]
+  mape[i] <-  mean(apk$apk)
+}
+
+mape
+
+
+
+
+
+
